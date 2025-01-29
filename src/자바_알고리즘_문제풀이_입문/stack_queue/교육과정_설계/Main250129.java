5479b6ead311e06af9ebcc12e67e72ed4cf67767
jongwanra <najongsi@gmail.com>
2025-01-29 12:23:54 +0900

    교육과정설계 구현

---
+++ src/자바_알고리즘_문제풀이_입문/stack_queue/교육과정_설계/Main250129.java
@@ -0,0 +1,41 @@
+package 자바_알고리즘_문제풀이_입문.stack_queue.교육과정_설계;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * 문제 링크: https://cote.inflearn.com/contest/10/problem/05-07
+ * 문제 접근 방법
+ * Queue를 이용해서 문제에 접근하자.
+ * 정상적으로 반목문이 종료될 경우 YES
+ * Queue가 먼저 비어버릴 경우 NO를 출력한다.
+ */
+public class Main250129 {
+	private String solution(String orderedSubjects, String subjects) {
+		Queue<Character> queue = new ArrayDeque<>();
+		for(char subject: subjects.toCharArray()){
+			queue.offer(subject);
+		}
+		int matchedCount = 0;
+		final int subjectsSize = orderedSubjects.length();
+
+		for(char orderedSubject: orderedSubjects.toCharArray()){
+			while(!queue.isEmpty() && matchedCount < subjectsSize) {
+				if(queue.poll() == orderedSubject){
+					matchedCount++;
+					break;
+				}
+
+			}
+		}
+		return matchedCount == subjectsSize ? "YES" : "NO";
+	}
+	public static void main(String[] args) throws IOException {
+		Main250129 main = new Main250129();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(main.solution(br.readLine(), br.readLine()));
+	}
+}
